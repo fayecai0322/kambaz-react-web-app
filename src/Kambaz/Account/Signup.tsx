f83501955ecb5b3365d72e351496fc3e289836d6
fayecai0322 <156372600+fayecai0322@users.noreply.github.com>
2025-01-15 20:50:26 -0800

    Updated Signup file

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Signup(){
     return(
-        <div>
-            <h3>Sign Up</h3>
-        </div>
+        <div id="wd-signup-screen">
+        <h3>Sign up</h3>
+        <input placeholder="username" id="wd-username" /> <br />
+        <input placeholder="password" id="wd-password" type="password" /> <br />
+        <input placeholder="Confirm password" id="wd-confirm-password" type="confirm password" /> <br />
+        <Link  to="/Kambaz/Dashboard" id="wd-signin-btn"> Sign in </Link><br />
+      </div>
     )
 };
