b5e7b791b7ad56ae4e75dd0dcefa3f2cb3561c1f
fayecai0322 <156372600+fayecai0322@users.noreply.github.com>
2025-01-25 11:11:47 -0800

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; {/*for drop down display */}
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"; {/*for drop down display */}
 import {App} from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
