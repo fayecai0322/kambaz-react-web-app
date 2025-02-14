b51fdca4298d48ed560dcb56c58aae4e6b05a989
fayecai0322 <156372600+fayecai0322@users.noreply.github.com>
2025-02-13 23:15:36 -0800

    second push

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -11,9 +11,25 @@ import ImpliedReturn from "./ImpliedRetunrs";
 import TemplateLiterals from "./TemplateLiterals";
 import SimpleArrays from "./SimpleArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
-import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import PathParameters from "./PathParameters";
 
 export default function Lab3(){
     return (
@@ -32,9 +48,25 @@ export default function Lab3(){
             <TemplateLiterals />
             <SimpleArrays />
             <ArrayIndexAndLength />
-            {/** Passing Arguments to Components*/}
-            <Add a={3} b={4} />
+            <AddingAndRemovingToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <FindFunction />
+            <FindIndex />
+            <FilterFunction />
+            <House />
+            <JsonStringify />
+            <TodoItem /> <hr />
+            <TodoList />
+            <Destructing />
+            <FunctionDestructing /> <hr />
+            <DestructingImports />
+            <Classes />
+            <Styles />
+
             {/**Passing parameter in body */}
+            <Add a={3} b={4} />
+
             <h4>Square of 4</h4>
             <Square>4</Square><hr />{/**use the component name */}
 
@@ -42,6 +74,7 @@ export default function Lab3(){
             <Highlight>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
             </Highlight>
+            <PathParameters /> <hr />
 
 
 
