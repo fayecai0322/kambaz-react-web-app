f1488d3832bef536321f8c6ba6e198fa57c7ec63
fayecai0322 <156372600+fayecai0322@users.noreply.github.com>
2025-03-27 22:55:55 -0700

    add cookie

--- src/Kambaz/Users/client.ts
+++ src/Kambaz/Users/client.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { User } from "./reducer";
 
-// 明确指定 response 类型是 { data: User[] }
+// ✅ 创建一个带 credentials 的 axios 实例
+const axioWithCredentials = axios.create({ withCredentials: true });
+
+// ✅ 明确指定 response 类型是 { data: User[] }
 export const fetchAllUsers = async (): Promise<User[]> => {
-  const response = await axios.get<User[]>("/api/users");
+  const response = await axioWithCredentials.get<User[]>("/api/users");
   return response.data;
 };
